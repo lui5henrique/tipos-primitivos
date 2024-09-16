4ae1f3e1c28eb2feca7aa786364b5b0e208c175a
Luis Henrique <lh_bigdata@outlook.com>
2024-09-16 22:40:19 +0000

    atualizando código

--- aula1.js
+++ aula1.js
@@ -1,1 +1,16 @@
 
+ola = "Hello Wordl"
+console.log(ola)
+
+rotulo = "natalya"
+console.log(rotulo)
+
+rotulo = 12345
+console.log(rotulo)
+
+const num1 = 10
+const num2 = 20
+const calc = num1 + num2
+console.log("O resultado é " + calc)
+
+
