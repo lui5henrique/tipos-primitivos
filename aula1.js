9c9935d43d55d7c38ac6ddd5e470436ab63d15aa
Luis Henrique <lh_bigdata@outlook.com>
2024-09-24 17:47:00 +0000

    Lista de exercícios

--- aula1.js
+++ aula1.js
@@ -1,19 +1,64 @@
 
-ola = "Hello Wordl"
-console.log(ola)
+//TIPOS PRIMITIVOS
 
-rotulo = "natalya"
-console.log(rotulo)
+var name = "Luis Henrique"
+var idade = 20
+console.log("Olá, meu nome é " + name + " e tenho "+ idade +" anos.")
 
-rotulo = 12345
-console.log(rotulo)
+var name = "Luis Henrique"
+var city = "Recife"
+console.log("" + name + " é de "+ city +".")
 
-const num1 = 10
-const num2 = 20
-const calc = num1 + num2
-console.log("O resultado é " + calc)
+var num1 = 50
+var num2 = 100
+var calc = num1 + num2
+console.log("O resultado da SOMA é " + calc)
 
-const num3 = 35
-console.log("num3")
+var num1 = 50
+var num2 = 100
+var calc = num1 - num2
+console.log("O resultado da SUBTRAÇÃO é " + calc)
 
+var num1 = 50
+var num2 = 100
+var calc = num1 * num2
+console.log("O resultado da MULTIPLICAÇÃO é " + calc)
+
+var num1 = 50
+var num2 = 100
+var calc = num1 / num2
+console.log("O resultado da DIVISÃO é " + calc)
+
+var base = 100
+var altura = 20
+var calc = base * altura
+console.log("A área do triangulo é " + calc)
+
+var nota1_Renato = 9.0
+var nota2_Renato = 9.5
+var nota3_Renato = 10
+var calc = (nota1_Renato + nota2_Renato + nota3_Renato) / 3
+console.log("O resultado da média é " + calc)
+
+var preco_original = 50
+var desconto = 0.50
+var calc = preco_original * desconto
+console.log("O preço com desconto ficará " + calc)
+
+//IMPOSTO DE RENDA - não consegui fazer.
+
+var valor_real = 100
+var cambio_dolar = 5.5
+var conversao = valor_real * cambio_dolar
+var valor_dolar = conversao
+console.log("O valor é R$" + valor_real + ", multiplicado pela taxa " + cambio_dolar + ", gerou um valor final em dolar de $" + conversao)
+
+var celsius = 29
+var fahrenheit = (celsius * 9.5) + 32
+console.log("O valor da temperatura conversido de Celsius para Fahrenheit é " + fahrenheit)
+
+var peso = 90
+var altura = 1.78
+var imc = peso / (altura * altura)
+console.log("O valor do IMC é " + imc)
 
